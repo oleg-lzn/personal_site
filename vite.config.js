9c5e71d2e379bbfe1c4ab14412dba145cf566346
Oleg <luzeninoleg2012@gmail.com>
2025-02-03 20:31:39 +0200

    settings change fix

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/", // add this field
+  optimizeDeps: {
+    include: ["react", "react-dom", "react/jsx-runtime"],
+  },
+  base: "/",
 });
